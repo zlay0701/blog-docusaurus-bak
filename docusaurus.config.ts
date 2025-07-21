98edf2137bb614cad3a02111b702f44b4968a4e3
zlay0701 <906105783@qq.com>
2025-07-21 12:25:00 +0800

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '标题',
+  tagline: '这是副标题',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
